46fe6d127dded8280d9d04dc2a3ccc788dd4ac9a
Tim <truecarry@gmail.com>
2022-04-14 03:16:09 +0300

    Add nft transfer

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -6,6 +6,9 @@ import { getProvider } from '../utils'
 import { AddressRow } from './AddressRow'
 import Popup from 'reactjs-popup'
 import { useEffect, useMemo, useState } from 'preact/hooks'
+import { HttpProvider } from 'tonweb/dist/types/providers/http-provider'
+
+const { NftItem } = TonWeb.token.nft
 
 function Wallet({
   wallet,
@@ -21,6 +24,9 @@ function Wallet({
   const [amount, setAmount] = useState('0')
   const [recepient, setRecepient] = useState('')
   const [seqno, setSeqno] = useState('0')
+  const [nft, setNft] = useState('')
+  const [nftRecepient, setNftRecepient] = useState('')
+  const [nftMessage, setNftMessage] = useState('')
 
   useEffect(() => {
     setAmount('0')
@@ -56,6 +62,24 @@ function Wallet({
       </div>
       <div>Balance: {walletBalance.result && TonWeb.utils.fromNano(walletBalance.result)}</div>
 
+      <div className="mt-2 flex flex-col">
+        <label htmlFor="amountInput">Seqno:</label>
+        <div>
+          <input
+            className="border rounded p-2"
+            id="amountInput"
+            type="text"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            value={seqno}
+            onChange={(e: any) => setSeqno(e.target.value)}
+          />
+          <button onClick={getSeqno} className="ml-2">
+            Get Seqno
+          </button>
+        </div>
+      </div>
+
       {/* Send ton */}
       <div className="flex flex-col">
         <div className="font-medium text-lg text-accent my-2">Send TON:</div>
@@ -84,58 +108,55 @@ function Wallet({
           />
         </div>
 
-        <div className="mt-2 flex flex-col">
-          <label htmlFor="amountInput">Seqno:</label>
-          <div>
-            <input
-              className="border rounded p-2"
-              id="amountInput"
-              type="text"
-              inputMode="numeric"
-              pattern="[0-9]*"
-              value={seqno}
-              onChange={(e: any) => setSeqno(e.target.value)}
-            />
-            <button onClick={getSeqno} className="ml-2">
-              Get Seqno
-            </button>
-          </div>
-        </div>
-
         <SendModal amount={amount} recepient={recepient} wallet={wallet} seqno={seqno} />
-        {/* <Modal trigger={} close={closeModal}>
-          <div className="flex flex-col p-4">
-            <div>
-              You will send {amount} TON to {recepient}.
-            </div>
-            <div className="mt-4">Are you sure?</div>
-            <div className="flex mt-2">
-              <div className="bg-highlight rounded px-2 py-2 text-white cursor-pointer">Yes</div>
-              <div
-                className="bg-highlight rounded px-2 py-2 text-white cursor-pointer ml-8"
-                onClick={closeModal}
-              >
-                Cancel
-              </div>
-            </div>
-          </div>
-        </Modal> */}
       </div>
 
       {/* Send nft */}
-      {/* <div className="flex flex-col mt-4">
-        <div className="font-medium text-lg text-accent my-2">Transfer NFT(WIP):</div>
+      <div className="flex flex-col mt-4">
+        <div className="font-medium text-lg text-accent my-2">Transfer NFT:</div>
 
         <div className="mt-2 flex flex-col">
           <label htmlFor="nftAddressInput">NFT Address:</label>
-          <input className="border rounded p-2" id="nftAddressInput" type="text" />
+          <input
+            className="border rounded p-2"
+            id="nftAddressInput"
+            type="text"
+            value={nft}
+            onChange={(e: any) => setNft(e.target.value)}
+          />
         </div>
 
         <div className="mt-2 flex flex-col">
           <label htmlFor="nftToInput">Recepient:</label>
-          <input className="border rounded p-2" id="nftToInput" type="text" />
+          <input
+            className="border rounded p-2"
+            id="nftToInput"
+            type="text"
+            value={nftRecepient}
+            onChange={(e: any) => setNftRecepient(e.target.value)}
+          />
         </div>
-      </div> */}
+
+        <div className="mt-2 flex flex-col">
+          <label htmlFor="nftMessageInput">Message:</label>
+          <input
+            className="border rounded p-2"
+            id="nftMessageInput"
+            type="text"
+            value={nftMessage}
+            onChange={(e: any) => setNftMessage(e.target.value)}
+          />
+        </div>
+
+        <SendNftModal
+          nft={nft}
+          recepient={nftRecepient}
+          wallet={wallet}
+          seqno={seqno}
+          provider={provider}
+          nftMessage={nftMessage}
+        />
+      </div>
     </div>
   )
 }
@@ -163,7 +184,6 @@ const SendModal = ({
     const result = await wallet.wallet.methods.transfer(params).send()
 
     console.log('result', result)
-    alert('sent money')
     close()
   }
 
@@ -179,6 +199,80 @@ const SendModal = ({
         <div className="flex flex-col p-4">
           <div>
             You will send {amount} TON to {recepient}.
+          </div>
+          <div className="mt-4">Are you sure?</div>
+          <div className="flex mt-2">
+            <div
+              className="bg-highlight rounded px-2 py-2 text-white cursor-pointer"
+              onClick={() => sendMoney(close)}
+            >
+              Yes
+            </div>
+            <div
+              className="bg-highlight rounded px-2 py-2 text-white cursor-pointer ml-8"
+              onClick={() => close()}
+            >
+              Cancel
+            </div>
+          </div>
+        </div>
+      )}
+    </Popup>
+  )
+}
+
+const SendNftModal = ({
+  nft,
+  recepient,
+  wallet,
+  seqno,
+  provider,
+  nftMessage,
+}: {
+  nft: string
+  recepient: string
+  wallet: IWallet
+  seqno: string
+  provider: HttpProvider
+  nftMessage: string
+}) => {
+  const sendMoney = async (close: () => void) => {
+    const nftAddress = new TonWeb.utils.Address(nft)
+    const amount = TonWeb.utils.toNano(0.05)
+    const nftItem = new NftItem(provider, { address: nftAddress })
+
+    const result = await wallet.wallet.methods
+      .transfer({
+        secretKey: wallet.key.secretKey,
+        toAddress: nftAddress,
+        amount: amount,
+        seqno: parseInt(seqno),
+        payload: await nftItem.createTransferBody({
+          newOwnerAddress: new TonWeb.utils.Address(recepient),
+          forwardAmount: TonWeb.utils.toNano(0.02),
+          forwardPayload: new TextEncoder().encode(nftMessage),
+          responseAddress: wallet.address,
+        }),
+        sendMode: 3,
+      })
+      .send()
+
+    console.log('result', result)
+    close()
+  }
+
+  return (
+    <Popup
+      trigger={
+        <button className="bg-highlight rounded px-2 py-2 w-48 text-white mt-2">Send</button>
+      }
+      modal
+      close={close}
+    >
+      {(close: () => void) => (
+        <div className="flex flex-col p-4">
+          <div>
+            You will send {nft} NFT to {recepient}.
           </div>
           <div className="mt-4">Are you sure?</div>
           <div className="flex mt-2">
