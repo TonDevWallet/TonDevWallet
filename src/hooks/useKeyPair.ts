ef202007bd71613328d6219af71cadc956f73206
Tim <truecarry@gmail.com>
2023-01-17 02:15:53 +0300

    Fix useKeyPair hook

--- src/hooks/useKeyPair.ts
+++ src/hooks/useKeyPair.ts
@@ -2,5 +2,5 @@ import { useMemo } from 'react'
 import { KeyPair, keyPairFromSeed } from 'ton-crypto'
 
 export function useKeyPair(seed?: string): KeyPair | undefined {
-  return useMemo(() => keyPairFromSeed(Buffer.from(seed || '', 'hex')), [seed])
+  return useMemo(() => (seed ? keyPairFromSeed(Buffer.from(seed, 'hex')) : undefined), [seed])
 }
