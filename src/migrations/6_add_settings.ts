725b0649f181aca1fa2fc77380232726e26a41fd
Tim <truecarry@gmail.com>
2023-02-06 03:42:39 +0300

    Persist testnet setting

---
+++ src/migrations/6_add_settings.ts
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.raw(`
+    CREATE TABLE settings (
+      name text PRIMARY KEY,
+      value text
+    )
+  `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.raw(`
+    DROP TABLE settings;
+  `)
+}
--- src/store/liteClient.ts
+++ src/store/liteClient.ts
@@ -2,14 +2,29 @@ import { LiteClient, LiteRoundRobinEngine, LiteSingleEngine } from 'ton-lite-cli
 import networkConfig from '@/networkConfig'
 import { hookstate, useHookstate } from '@hookstate/core'
 import { tauriState } from './tauri'
+import { getDatabase } from '@/db'
 
 const LiteClientState = hookstate<{
   liteClient: LiteClient
   testnet: boolean
 }>(async () => {
+  const db = await getDatabase()
+  const testnetSetting = await db<{ name: string; value: string }>('settings')
+    .where('name', 'is_testnet')
+    .first()
+
+  if (!testnetSetting) {
+    await db('settings').insert({
+      name: 'is_testnet',
+      value: 'false',
+    })
+  }
+
+  const isTestnet = testnetSetting?.value === 'true'
+
   return {
-    testnet: false,
-    liteClient: await getLiteClientAsync(false),
+    testnet: isTestnet,
+    liteClient: await getLiteClientAsync(isTestnet),
   }
 })
 
@@ -32,125 +47,13 @@ export async function changeLiteClient(testnet: boolean) {
     testnet,
     liteClient: newLiteClient,
   })
-}
 
-function getTempClient() {
-  interface queueItem {
-    method: string
-    args: unknown[]
-    resolve: () => void
-    reject: () => void
-  }
+  const db = await getDatabase()
+  await db<{ name: string; value: string }>('settings')
+    .where('name', 'is_testnet')
+    .update('value', String(testnet))
 
-  const queue: queueItem[] = []
-  let localClient: LiteClient | undefined
-  let clientResolved = false
-
-  const createShim = (name: string) => {
-    return (...args: unknown[]) => {
-      if (clientResolved && localClient) {
-        return localClient[name](...args)
-      }
-
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      let _resolve: () => void = () => {}
-      // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-explicit-any
-      let _reject: (reason?: any) => void = () => {}
-
-      const p = new Promise<void>((resolve, reject) => {
-        _resolve = resolve
-        _reject = reject
-      })
-
-      queue.push({
-        method: name,
-        args: [...args],
-        resolve: _resolve,
-        reject: _reject,
-      })
-
-      return p
-    }
-  }
-  const tempWait = {}
-  for (const name of [
-    'getMasterchainInfo',
-    'getAccountState',
-    'getAccountTransactions',
-    'sendMessage',
-    'getMasterchainInfoExt',
-    'getCurrentTime',
-    'getVersion',
-    'getConfig',
-    'getAccountTransaction',
-    'runMethod',
-    'lookupBlockByID',
-    'getBlockHeader',
-    'getAllShardsInfo',
-    'listBlockTransactions',
-    'getFullBlock',
-    // 'engine',
-  ]) {
-    tempWait[name] = createShim(name)
-  }
-
-  localClient = tempWait as LiteClient
-
-  const endWait = (client: LiteClient) => {
-    localClient = client
-    clientResolved = true
-
-    LiteClientState.merge({
-      liteClient: localClient,
-    })
-    for (const item of queue) {
-      // console.log('item work', item, lc)
-      if (client && client[item.method]) {
-        client[item.method](...item.args)
-          .then(item.resolve)
-          .catch(item.reject)
-      }
-    }
-  }
-
-  return {
-    tmpClient: localClient as LiteClient,
-    endWait,
-  }
-}
-
-function getLiteClient(isTestnet: boolean): LiteClient {
-  const { tmpClient, endWait } = getTempClient()
-  console.log('getLiteClient', isTestnet)
-  setTimeout(async () => {
-    const data = isTestnet ? networkConfig.testnetConfig : networkConfig.mainnetConfig
-
-    const tauri = (await tauriState.promise) || tauriState
-    if (!tauri) {
-      console.log('no tauri', tauri)
-      return
-    }
-
-    const engines: LiteSingleEngine[] = []
-    for (const ls of shuffle(data.liteservers).slice(0, 3)) {
-      const pubkey = encodeURIComponent(ls.id.key)
-      engines.push(
-        new LiteSingleEngine({
-          // host: `wss://ws.tonlens.com/?ip=${ls.ip}&port=${ls.port}&pubkey=${pubkey}`,
-          host: `ws://localhost:${tauri.port.get()}/?ip=${ls.ip}&port=${ls.port}&pubkey=${pubkey}`,
-          publicKey: Buffer.from(ls.id.key, 'base64'),
-          client: 'ws',
-        })
-      )
-    }
-
-    const engine = new LiteRoundRobinEngine(engines)
-    const client = new LiteClient({ engine })
-
-    await client.getMasterchainInfo()
-    endWait(client)
-  }, 0)
-  return tmpClient
+  console.log('update done')
 }
 
 export async function getLiteClientAsync(isTestnet: boolean): Promise<LiteClient> {
