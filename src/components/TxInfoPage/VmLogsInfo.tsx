802d4e955ff59d4d85f3e075b8d835ab2b730f06
Tim <truecarry@gmail.com>
2023-05-28 02:54:17 +0300

    Better instruction parsing

--- src/components/TxInfoPage/VmLogsInfo.tsx
+++ src/components/TxInfoPage/VmLogsInfo.tsx
@@ -4,21 +4,32 @@ export function VmLogsInfo({ logs, setStack }: { logs: string; setStack: (string
   const logsData = useMemo(() => {
     const instructions = logs.split('stack: ').slice(1)
     const commands = instructions.map((instruction) => {
-      const lines = instruction.split('\n')
+      const lines = instruction.split('\n').filter((l) => l)
 
       const stack = lines[0]
-      const codeHash = lines[1]
-      const cmd = lines[2].split('execute ')[1]
+
+      let cmd = ''
       let gas = ''
       let exception = ''
       let exceptionHandler = ''
+      let codeHash = ''
 
-      if (lines.length === 5) {
-        gas = lines[3].split('gas remaining: ')[1]
-      } else {
-        exception = lines[3]
-        exceptionHandler = lines[4]
+      for (const line of lines.slice(1)) {
+        if (line.startsWith('gas remaining: ')) {
+          gas = line.split('gas remaining: ')[1]
+        } else if (line.startsWith('execute ')) {
+          cmd = line.split('execute ')[1]
+        } else if (line.startsWith('code cell hash: ')) {
+          codeHash = line.split('code cell hash: ')[1]
+        } else if (line.startsWith('default exception handler, ')) {
+          exceptionHandler = line.split('default exception handler, ')[1]
+        } else if (line.startsWith('handling exception code ')) {
+          exception = line.split('handling exception code ')[1]
+        } else {
+          console.log('unknown line', line, instruction)
+        }
       }
+
       return {
         stack,
         codeHash,
@@ -26,6 +37,7 @@ export function VmLogsInfo({ logs, setStack }: { logs: string; setStack: (string
         gas,
         exception,
         exceptionHandler,
+        raw: instruction,
       }
     })
 
@@ -44,7 +56,6 @@ export function VmLogsInfo({ logs, setStack }: { logs: string; setStack: (string
         {logsData.map((command, i) => {
           const prevCommand = logsData[i - 1]
           const gas = parseInt(prevCommand?.gas || '1000000') - parseInt(command.gas)
-          console.log(command, Number(prevCommand?.gas || 10000000), 'a', parseInt(command.gas))
           return (
             <div className="grid grid-cols-[60px_1fr_60px] h-6" key={i}>
               <div>{i}.</div>
@@ -57,7 +68,7 @@ export function VmLogsInfo({ logs, setStack }: { logs: string; setStack: (string
                 {command.cmd}
               </div>
               <div className="text-right">{gas || ''}</div>
-              {command.exception && (
+              {(command.exception || command.exceptionHandler) && (
                 <div className="col-span-3">
                   <div>exception: {command.exception}</div>
                   <div>exceptionHandler: {command.exceptionHandler}</div>
