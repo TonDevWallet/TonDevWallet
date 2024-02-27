057afe1a03dc9f95ab605c01d60f15ad377cf073
Tim <truecarry@gmail.com>
2024-02-27 04:41:42 +0300

    Update liteclient, cleanup, add timeouts to rust tcp connection

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,27 +1,6 @@
-// import { invoke } from '@tauri-apps/api'
 import { appWindow, Theme } from '@tauri-apps/api/window'
 import { useState, useEffect } from 'react'
 import useLocalStorageState from 'use-local-storage-state'
-
-// function numberToRgba(x: number) {
-//   return [(x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, (x & 0xff) / 255]
-// }
-
-async function setColors() {
-  // const colors = (await invoke('get_system_colors')) as { [key: string]: number }
-  //
-  // const root = document.documentElement
-  // for (const key of Object.keys(colors)) {
-  //   if (colors[key]) {
-  //     const [r, g, b, a] = numberToRgba(colors[key])
-  //
-  //     const cssKey = key.replace(/_/g, '-')
-  //
-  //     // root.style.setProperty(`--color-${cssKey}`, `rgba(${r}, ${g}, ${b}, ${a})`)
-  //     // root.style.setProperty(`--color-${cssKey}-rgb`, `${r} ${g} ${b}`)
-  //   }
-  // }
-}
 
 export const useTheme = (): [Theme, (v: Theme) => void] => {
   const [theme, setThemeValue] = useState<Theme>('light')
@@ -52,7 +31,6 @@ export const useTheme = (): [Theme, (v: Theme) => void] => {
     ;(async () => {
       if (!localTheme) {
         setTheme((await appWindow.theme()) || 'light', false)
-        setColors().then()
       }
 
       unlisten = await appWindow.onThemeChanged(({ payload: theme }) => {
@@ -61,7 +39,6 @@ export const useTheme = (): [Theme, (v: Theme) => void] => {
         }
         console.log(`theme changed to ${theme}`)
         setTheme(theme, false)
-        setColors()
       })
     })()
 
