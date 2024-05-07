bb6377ee95781b4ea0c1f42497d85edc6b3e75c3
Tim <truecarry@gmail.com>
2024-05-07 08:10:04 +0300

    Remove analyzer from config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
 import inject from '@rollup/plugin-inject'
-import { analyzer } from 'vite-bundle-analyzer'
+// import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [
     react(),
-    analyzer({
-      analyzerMode: 'static',
-    }),
+    // analyzer({
+    //   analyzerMode: 'static',
+    // }),
   ],
   server: {
     port: 3003,
